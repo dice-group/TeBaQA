d3ef26fb45b00e48d5b9839577b6ccf510624fe5
Peter Nancke <peter.nancke@gmx.de>
2017-12-04 15:12:36 +0100

    Save properties and ontology nodes

---
+++ src/main/java/de/uni/leipzig/tebaqa/helper/DBpediaPropertiesProvider.java
@@ -0,0 +1,41 @@
+package de.uni.leipzig.tebaqa.helper;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+public class DBpediaPropertiesProvider {
+    private static List<String> properties = new ArrayList<>();
+
+    //do not instantiate
+    private DBpediaPropertiesProvider() {
+    }
+
+    public static List<String> getDBpediaProperties() {
+        if (properties.isEmpty()) {
+            properties = requestDBpediaProperties();
+        }
+        return properties;
+    }
+
+    private static List<String> requestDBpediaProperties() {
+        Set<String> properties = new HashSet<>();
+        String query = "select ?property where { ?property a <http://www.w3.org/1999/02/22-rdf-syntax-ns#Property> } OFFSET %d LIMIT 10000";
+        boolean gotResult = true;
+        int offset = 0;
+        while (gotResult) {
+            String format = String.format(query, offset);
+            List<String> result = SPARQLUtilities.executeSPARQLQuery(format).getResultSet();
+            if (!result.isEmpty()) {
+                properties.addAll(result);
+                offset += 10000;
+            } else {
+                gotResult = false;
+            }
+        }
+        return newArrayList(properties);
+    }
+}
