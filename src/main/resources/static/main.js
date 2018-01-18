1b65709a78fd466cfc7809a1054a17769e03c859
Peter Nancke <peter.nancke@gmx.de>
2018-01-18 21:24:42 +0100

    Add Better Date format in answer; Better exclusion of stopwords

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -13,9 +13,13 @@ function emptyAnswerList() {
 }
 
 function createInfobox(msg) {
+    if (!isNaN(Date.parse(msg.title))) {
+        msg.title = new Date(Date.parse(msg.title)).toLocaleDateString();
+    }
+
     let divTemplate = '<div class="card"><div class="summary">';
     if (msg.hasOwnProperty('image')) {
-        divTemplate += '<div class="img-wrapper"><img src="?image"></div><div class="content">'.replace('?image', msg.image);
+        divTemplate += '<div class="img-wrapper"><img src="?image" class="wiki-image"></div><div class="content">'.replace('?image', msg.image);
     } else {
         divTemplate += '<div class="content-no-image">';
     }
@@ -127,7 +131,46 @@ function submitForm(s) {
     });
 }
 
+function initExamples() {
+    $('#example-1').click(function (e) {
+        e.preventDefault();
+        $('#search-bar').val('Where is Angela Merkel born?');
+        return false;
+    });
+    $('#example-2').click(function (e) {
+        e.preventDefault();
+        $('#search-bar').val('What is the wavelength of indigo?');
+        return false;
+    });
+    $('#example-3').click(function (e) {
+        e.preventDefault();
+        $('#search-bar').val('When was the death of Shakespeare?');
+        return false;
+    });
+    $('#example-4').click(function (e) {
+        e.preventDefault();
+        $('#search-bar').val('Where is the birthplace of Goethe?');
+        return false;
+    });
+    $('#example-5').click(function (e) {
+        e.preventDefault();
+        $('#search-bar').val('Where is the origin of Carolina reaper?');
+        return false;
+    });
+    $('#example-6').click(function (e) {
+        e.preventDefault();
+        $('#search-bar').val('Who is the author of the interpretation of dreams?');
+        return false;
+    });
+    $('#example-7').click(function (e) {
+        e.preventDefault();
+        $('#search-bar').val('Give me all members of the The Prodigy!');
+        return false;
+    });
+}
+
 function init() {
+    initExamples();
     hideSpinner();
     $('#search-form-id').submit(function () {
         submitForm($('#search-bar').val());
