ee7c8016b10073ee5a5444e8a8e493ebc1064483
Daniel Vollmers <vdaniel@mail.upb.de>
2020-07-07 10:44:21 +0200

    integrate keyword search

---
+++ src/main/java/de/uni/leipzig/tebaqa/controller/KeyWordController.java
@@ -0,0 +1,37 @@
+package de.uni.leipzig.tebaqa.controller;
+
+import de.uni.leipzig.tebaqa.model.ResourceCandidate;
+import org.apache.jena.sparql.algebra.Op;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class KeyWordController {
+    private ElasticSearchEntityIndex index;
+    public KeyWordController(){
+        try {
+            index=new ElasticSearchEntityIndex();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Set<ResourceCandidate> searchByKeywords(String searchString, Optional<String>restrict
+            ,Optional<String>type,Optional<String>connectedProperty,Optional<String>connectedResource){
+        Set<ResourceCandidate>candidates=new HashSet<>();
+        restrict.ifPresent(s->{
+            if(s.equals("entity"))candidates.addAll(index.searchEntity(searchString,connectedResource,connectedProperty,type));
+            else if(s.equals("property"))candidates.addAll(index.searchResource(searchString,"property",false));
+            else candidates.addAll(index.searchResource(searchString,"class",false));
+        });
+        if(!restrict.isPresent()){
+            candidates.addAll(index.searchEntity(searchString,connectedResource,connectedProperty,type));
+            candidates.addAll(index.searchResource(searchString,"property",false));
+            candidates.addAll(index.searchResource(searchString,"class",false));
+        }
+        return candidates;
+    }
+    
+}
--- src/main/java/de/uni/leipzig/tebaqa/model/AnswerToQuestion.java
+++ src/main/java/de/uni/leipzig/tebaqa/model/AnswerToQuestion.java
@@ -18,7 +18,24 @@ public class AnswerToQuestion {
         this.sparqlQuery = answer.getQuery();
         this.rdfEntities = entitiyToQuestionMapping;
 
-        if (!this.answer.isEmpty() && this.answer.parallelStream().allMatch(a -> a.startsWith("http://dbpedia.org/"))) {
+        if (!this.answer.isEmpty() && this.answer.parallelStream().allMatch(a -> a.startsWith("http://"))) {
+            this.answerType = "uri";
+        } else {
+            //NO uri but date, string or number
+            if (this.answer.parallelStream().allMatch(StringUtils::isNumeric)) {
+                this.answerType = "number";
+            } else {
+                this.answerType = "literal";
+            }
+        }
+    }
+    public AnswerToQuestion(Set<ResourceCandidate> answers) {
+        this.answer = new HashSet<>();
+        answers.forEach(as->answer.add(as.getUri()));
+        this.sparqlQuery = "";
+        this.rdfEntities = null;
+
+        if (!this.answer.isEmpty() && this.answer.parallelStream().allMatch(a -> a.startsWith("http://"))) {
             this.answerType = "uri";
         } else {
             //NO uri but date, string or number
