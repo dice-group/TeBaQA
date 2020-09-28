2c53cc81bab9c4f74c9f818ca66cde47e11ab5de
toardik <hardik.topiwala@gmail.com>
2020-09-28 19:34:13 +0200

    Modularization init

--- entity-linking/src/main/java/de/uni/leipzig/tebaqa/entitylinking/EntityLinkingApplication.java
+++ entity-linking/src/main/java/de/uni/leipzig/tebaqa/entitylinking/EntityLinkingApplication.java
@@ -1,13 +1,24 @@
 package de.uni.leipzig.tebaqa.entitylinking;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class EntityLinkingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EntityLinkingApplication.class, args);
-	}
+    public static Logger LOGGER = Logger.getRootLogger();
 
+
+    public static void main(String[] args) {
+        LOGGER.info("Entity linking server started");
+        SpringApplication.run(EntityLinkingApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        LOGGER.info("Entity linking server started");
+    }
 }
--- template-classification/src/main/java/de/uni/leipzig/tebaqa/template/ClassificationApplication.java
+++ template-classification/src/main/java/de/uni/leipzig/tebaqa/template/ClassificationApplication.java
@@ -1,13 +1,17 @@
 package de.uni.leipzig.tebaqa.template;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClassificationApplication {
 
+    public static Logger LOGGER = Logger.getRootLogger();
+
+
     public static void main(String[] args) {
+        LOGGER.info("Template classification server started");
         SpringApplication.run(ClassificationApplication.class, args);
     }
-
 }
